b61c1edbc4df16b775112acba8c236c1a5d6136b
Pavel Zaitsev <mefmor@gmail.com>
2022-06-09 20:29:41 +0300

    Add console input to unittests

--- src/main/java/net/mefmor/sample/Main.java
+++ src/main/java/net/mefmor/sample/Main.java
@@ -1,7 +1,13 @@
 package net.mefmor.sample;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What is your name? >");
+        String name = sc.nextLine();
+        System.out.printf("%nHello, %s!", name);
     }
 }
